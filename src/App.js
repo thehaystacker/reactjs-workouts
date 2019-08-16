f9b83b2832e74bed135518394bda00f800eed243
thehaystacker <thehaystacker.in@gmail.com>
2019-08-16 17:05:02 +0530

    Understanding state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-import Person from './Person/Person.js';
+import Person from "./Person/Person.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Person name="Sangeeth" age="24" />
-      <Person name="Sarath" age="20" />
-      <Person>{['Football', 'hockey', 'Video Games']}</Person>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [
+      {name: 'Sangeeth', age: 24},
+      {name: 'Sarath', age: 20},
+      {name: 'Jithu', age: 28},
+    ]
+  }
+  
+  render() {
+    let Persons = null;
+    if (this.state.users && this.state.users.length) {
+      Persons = this.state.users.map((person, i) => {
+        return (
+          <Person name={person.name} age={person.age} key={i} />
+        )
+      })
+    }
+    
+    return (
+      <div className="App">
+        {Persons}
+      </div>
+    );
+  }
 }
 
 export default App;
